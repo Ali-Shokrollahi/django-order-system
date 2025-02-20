e3d63470cd15451e9650e465b9dc19b66f85974d
Ali Shokrollahi <ali.shokrollahi.pro@gmail.com>
2025-02-20 16:27:50 +0330

    Implement custom user model

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_active",)
+    list_filter = ("role", "is_active", "created_at")
+    fieldsets = (
+        ("Auth", {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("role", "is_verified", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2",
+                "is_active", "groups", "user_permissions"
+            )}
+         ),
+    )
+    search_fields = ("email",)
+    ordering = ("created_at",)
+
+
+admin.site.register(User, CustomUserAdmin)
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+from apps.utils.models import TimeStampModel
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email.lower()),
+        )
+
+        if password is not None:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.full_clean()
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            password=password,
+        )
+
+        user.is_superuser = True
+        user.is_active = True
+        user.is_verified = True
+        user.role = self.model.RoleChoices.ADMIN
+        user.save(using=self._db)
+
+        return user
+
+
+class User(TimeStampModel, AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model that uses email instead of username.
+    """
+
+    class RoleChoices(models.TextChoices):
+        CUSTOMER = "customer"
+        SELLER = "seller"
+        ADMIN = "admin"
+
+    email = models.EmailField(verbose_name="email address", unique=True)
+
+    role = models.CharField(
+        max_length=10, choices=RoleChoices, default=RoleChoices.CUSTOMER
+    )
+
+    is_active = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.role == self.RoleChoices.ADMIN
