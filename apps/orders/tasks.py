525c84e94e2869eccb76fe2c120ec14ce8e55e68
Ali Shokrollahi <ali.shokrollahi.pro@gmail.com>
2025-04-03 21:53:45 +0330

    improve task configs

--- apps/orders/tasks.py
+++ apps/orders/tasks.py
@@ -1,16 +1,17 @@
 from celery import shared_task
+from celery.utils.log import get_task_logger
 from django.core.mail import EmailMessage
 from apps.orders.invoices import InvoicePDFGenerator
 from apps.orders.repositories import InvoiceRepository, OrderRepository
 
+logger=get_task_logger(__name__)
 
 @shared_task(
+    bind=True,
     max_retries=3,
     default_retry_delay=60,
-    retry_backoff=True,
-    retry_jitter=True,
 )
-def generate_and_save_invoice(order_id: int):
+def generate_and_save_invoice(self, order_id: int):
     """Generate and save the invoice PDF for the given order."""
 
     try:
@@ -20,17 +21,17 @@ def generate_and_save_invoice(order_id: int):
         pdf_buffer.close()
         return f"Invoice generated for Order {order_id}"
     except Exception as e:
-        print(f"Error generating invoice for Order {order_id}: {e}")
-        generate_and_save_invoice.retry(exc=e)
+        logger.error(f"Error generating invoice for Order {order_id}: {e}")
+        # Retry the task if it fails
+        self.retry()
 
 
 @shared_task(
+    bind=True,
     max_retries=3,
     default_retry_delay=60,
-    retry_backoff=True,
-    retry_jitter=True,
 )
-def send_order_confirmation_email(order_id, recipient_email):
+def send_order_confirmation_email(self,order_id, recipient_email):
     try:
         invoice = InvoiceRepository().get_invoice_by_order_id(order_id)
 
@@ -45,5 +46,6 @@ def send_order_confirmation_email(order_id, recipient_email):
         email.send()
         return f"Email sent for Order {order_id}"
     except Exception as e:
-        print(f"Error sending email for Order {order_id}: {e}")
-        send_order_confirmation_email.retry(exc=e)
+        logger.error(f"Error sending email for Order {order_id}: {e}")
+        # Retry the task if it fails
+        self.retry()
